68878f7d0149dd8f80b80cbff82b263a99258961
GAAzevedo <guilherme.alvaresa@Gmail.com>
2024-11-01 10:41:50 -0300

    Cookies AdOpt

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useEffect } from "react";
 import Footer from "./components/Footer/Footer";
 import WhatsApp from "./components/WhatsApp/WhatsApp";
 import Header from "./components/Header/Header";
@@ -9,35 +7,13 @@ import './App.css';
 
 const App = () => {
   const { pathname } = useLocation();
-  const [activatedCookies, setActivatedCookies] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  useEffect(() => {
-    let cookies = localStorage.getItem("cookies");
-    console.log(cookies);
-
-    if (cookies) {
-      setActivatedCookies(true);
-    }
-  }, []);
-
-  const handleCookies = () => {
-    localStorage.setItem("cookies", true);
-    setActivatedCookies(true);
-  };
-
   return (
     <>
-      {!activatedCookies && (
-        <div className="cookies">
-          <p className="mb-2 mb-lg-0 text-center">Nosso site utiliza cookies para aprimorar sua experiência de navegação.</p>
-          <button className="btn btn-danger" onClick={handleCookies}>Prosseguir</button>
-        </div>
-      )}
-
       <Header />
       <main id="content" className="m-start bg-green-light">
         <Outlet />
